f195dcdebaebe9b571395bcf71b3e25181b14035
Hroddric <rodrigo.perezordoyobellido@gmail.com>
2018-06-19 09:49:22 +0100

    Created the functions file, containing some little useful funcs

---
+++ functions.py
@@ -0,0 +1,20 @@
+# System import
+import sys
+
+def dict_bash_kwargs():
+    """
+    This function returns a dictionary containing the keyword arguments given in bash as 
+    a dictionary for better handling. The arguments in bash should have the following 
+    form: argument1=something . If there are ANY spaces there, the computer will interpret
+    it as different arguments. One can also put strings like arg="here we can use spaces".
+    """
+
+    args = []
+    for i in range(len(sys.argv) - 1):
+        arg = str(sys.argv[i+1]).split("=")
+        args.append(arg)
+    return dict(args)
+
+
+def identity(time, signal):
+    return signal, True
