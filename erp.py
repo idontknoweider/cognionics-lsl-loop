0af65f8d068ea9293cf2a477e8c4a55e2fbab981
Hroddric <rodrigo.perezordoyobellido@gmail.com>
2018-07-16 15:31:46 +0100

    Testing datasets. Finding errors when fitting with LDA.

--- erp.py
+++ erp.py
@@ -6,6 +6,7 @@ import glob
 import sklearn
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from functions import rowcol_paradigm
+import random
 
 ## MAIN ##
 # First we are going to load every ERP data file
@@ -18,87 +19,58 @@ for file_ in path_list:
 # Create a list containing the characters used in the speller
 chars = rowcol_paradigm()
 
+def dataset_probe(dataset):
+    print("! DATASET: !")
 
-erp1 = erp_data_list[1]
-erp_ = erp1.train_data
-data = erp_["features"]
-rowcol = erp_["rowcol"]
-flags = erp_["flags"]
-# print("Type of the erp1 train data: {0}".format(type(erp_)))
-# print("This should be lenght 3 for having features, rowcol and flag: {0}".format(len(erp_)))
-# print("This should be the length of the feature stuff: {0}".format(len(data)))
-# print("This should be the length of the rowcol and labels stuff which should be similar to the thing before: {0} ; {1}".format(len(rowcol), len(flags)))
-# print("This is the length of a single feature array: {0} {1}".format(len(data), len(data[0])))
-# print("Last item of rowcol and flags are: {0}, {1}".format(rowcol[-1], flags[-1]))
+    feat = dataset["features"]
+    rc = dataset["rowcol"]
+    flg = dataset["flags"]
 
-# for i in range(12):
-#     print("Data item number {0} has length {1}".format(i, len(data[i])))
+    # Check the dataset's characteristics and print them out
+    print("Length of features, rowcol and flags: {0}, {1}, {2}".format(len(feat), len(rc), len(flg)))
+    print("Length of the first 5 feature arrays:")
+    for i in range(5):
+        print("\t Data item number {0} has length {1}".format(i, len(feat[i])))
+    print("\t Last data item number has length {0}".format(len(feat[-1])))
+    print("\n")
 
-# sum_ = sum(num > 12 for num in rowcol)
-# sum_2 = sum(num == 0 for num in rowcol)
-# print("There are {0} values inside rowcol that are larger than 12. WHAT. \n But there also are {1} values that are 0.".format(sum_, sum_2))
-# zeros = [len(data[i+1]) for i in range(len(rowcol)-1) if rowcol[i+1] == 0]
-# avg_len = sum(zeros)/len(zeros)
-# print("The avg length of the items with zeroes as rowcol is {0}".format(avg_len))
+# Load random dataset from the list and probe the training and test sets
+erp_set = erp_data_list[random.randint(0,len(erp_data_list))]
 
-# triggers = sum(flags)
-# print("The number of target triggers is: {0}".format(triggers))
-# print("Len last data: {0}".format(len(data[-1])))
-triggers_ = np.asarray(rowcol)[np.asarray(flags) == 1]
+trdat = erp_set.train_data
+dataset_probe(trdat)
 
-print(triggers_)
-print(len(triggers_))
+tsdat = erp_set.test_data
+dataset_probe(tsdat)
 
+# Separate features, rowcol and flags
+trfeat = trdat["features"]
+trrc = trdat["rowcol"]
+trflg = trdat["flags"]
 
-string = ""
+tsfeat = tsdat["features"]
+tsrc = tsdat["rowcol"]
+tsflg = tsdat["flags"]
 
-for i in np.arange(0, len(triggers_), 2):
-    inda = int(triggers_[i])
-    indb = int(triggers_[i+1])
-    if inda <= indb:
-        string_ex = chars[indb-7][inda-1]
-    else:
-        string_ex = chars[inda-7][indb-1]
-    if string == "" or string[-1] != string_ex:
-        string += string_ex
-print(string)
+# Select the correct data from the features list. Take the ones with the largest ammount of features
+std_len = len(trfeat[2])    # Standard length
+std_bool_list = np.asarray([len(item) for item in trfeat]) == std_len
+std_trfeat = np.asarray(trfeat)[std_bool_list]
+std_trflg = np.asarray(trflg)[std_bool_list]
+std_tsfeat = np.asarray(tsfeat)[std_bool_list]
+std_tsflg = np.asarray(tsflg)[std_bool_list]
 
-# Choose train data
-boolarray = (np.asarray([len(item) for item in data]) == 396)
+# Check the length of these fellow arrays
+print("Length of the standarised training feature and flags arrays: {0} ; {1}".format(len(std_trfeat), len(std_trflg)))
+print("Length of the first item of the std training feature list: {0}".format(len(std_trfeat[0])))
+print("Type of the flags: {0}".format(type(std_trflg[0])))
+print("Length of the standarised test feature and flags arrays: {0} ; {1}".format(len(std_tsfeat), len(std_tsflg)))
+print("Length of the first item of the std test feature list: {0}".format(len(std_tsfeat[0])) + "\n")
 
-print([len(item) for item in data[2::2]])
-print(sum(np.asarray([len(item) for item in data[2::2]]) > 400))
+for i in range(len(std_trfeat)):
+    if len(std_trfeat[i]) != std_len:
+        raise ValueError("WARNING: SOMETHING IS WRONG WITH DIMENSIONS")
 
-
-train_data = np.asarray(data)[boolarray]
-train_flags = np.asarray(flags)[boolarray]
-
-print(sum(np.asarray([len(item) for item in train_data]) != 396))
-
-erp_tst = erp1.test_data
-tstdata = erp_tst["features"]
-tstrowcol = erp_tst["rowcol"]
-tstflags = erp_tst["flags"]
-
-print("Type of the erp1 train data: {0}".format(type(erp_tst)))
-print("This should be lenght 3 for having features, rowcol and flag: {0}".format(
-    len(erp_tst)))
-print("This should be the length of the feature stuff: {0}".format(
-    len(tstdata)))
-print("This should be the length of the rowcol and labels stuff which should be similar to the thing before: {0} ; {1}".format(
-    len(tstrowcol), len(tstflags)))
-print("This is the length of a single feature array: {0} {1}".format(
-    len(tstdata), len(tstdata[0])))
-print("Last item of rowcol and flags are: {0}, {1}".format(
-    tstrowcol[-1], tstflags[-1]))
-
-for i in range(12):
-    print("Data item number {0} has length {1}".format(i, len(tstdata[i])))
-
-print("data len: {0} ; data flags: {1}".format(
-    len(train_data), len(train_flags)))
-# LDA STUFF
+# Create LDA model
 lda = LDA()
-bla = np.asarray(train_data)
-ble = np.asarray(train_flags)
-lda.fit(train_data, train_flags)
+lda.fit(np.asarray(std_trfeat), np.asarray(std_trflg))
