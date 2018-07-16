4be90a0ee28cd541de6f673f547e89bf22adb459
Hroddric <rodrigo.perezordoyobellido@gmail.com>
2018-07-16 14:52:05 +0100

    Added rowcol_paradigm to help defining the speller's layout. Formatting.

--- functions.py
+++ functions.py
@@ -17,7 +17,7 @@ def dict_bash_kwargs():
     return dict(args)
 
 
-def erp_into_chunks(erp_array, concatenate = False):
+def erp_into_chunks(erp_array, concatenate=False):
     """
     This function is used to change the format of the ERP data from the
     dataset used to train LDA and networks. It takes the (#channels
@@ -37,7 +37,7 @@ def erp_into_chunks(erp_array, concatenate = False):
             sequences). First one contains lists with the features
     """
 
-    # Initialize the lists that are going to hold all the 
+    # Initialize the lists that are going to hold all the
     features = []
     rowcol = []
     flags = []
@@ -47,37 +47,60 @@ def erp_into_chunks(erp_array, concatenate = False):
 
     # Start the iterations
     for i in range(erp_array.shape[1]):
-
-        # Create a list to hold each set of features if required
-        if init_loop == True:
-            features_temp = [[] for i in range(9)] # this is going to hold all the channels
-            rowcol_temp = []
-            flags_temp = []
-            init_loop = False
-
         # Extract the information from the erp_array
         data = erp_array[0:9, i]
         rowcol_ind = erp_array[9, i]
         flag = erp_array[10, i]
 
         # If there's data in the temp list and it is a different rowcol, store it as a feature vector
-        if rowcol_temp != [] and rowcol_ind != rowcol_temp[-1]:
+        if rowcol != [] and rowcol_ind != rowcol[-1]:
+            # Set flag to reset temp lists
             init_loop = True
+
             # In case we want the features to be concatenated
             if concatenate == True:
                 concatenation = []
                 for i in range(len(features_temp)):
                     concatenation.extend(features_temp[i])
                 features_temp = concatenation
-            
-            features.append(features_temp)
-            rowcol.append(rowcol_temp)
-            flags.append(flags_temp)
-        else:
-            for i in range(len(features_temp)):
-                features_temp[i].append(data[i])
-            rowcol_temp.append(rowcol_ind)
-            flags_temp.append(flag)
 
-    # Return a dictionary with the feature vectors and so on.
-    return {"features": features, "rowcol": rowcol, "flags": flags}
+            # Save temp lists to final lists
+            features.append(features_temp)
+
+        # Initialize temporary lists
+        if init_loop == True:
+            # this is going to hold all the channels
+            features_temp = [[] for i in range(9)]
+            init_loop = False
+
+            # This is used to not repeat the same rowcol or flag over and over in the list
+            first_flag = True
+
+        # Add the data to the temp lists
+        for i in range(len(features_temp)):
+            features_temp[i].append(data[i])
+
+        # If first data sample in sequence, save rowcol and flag
+        if first_flag == True:
+            rowcol.append(rowcol_ind)
+            flags.append(flag)
+            first_flag = False
+
+    # Return a dictionary with the feature vectors and so on. Rowcol and
+    #   flags have an extra value due to the implementation.
+    return {"features": features, "rowcol": rowcol[:-1], "flags": flags[:-1]}
+
+
+def rowcol_paradigm():
+    """
+    This function defines a list containing the rowcol paradigm to make
+    it easier to decypher using only indices for the columns and
+    rows from 1 to 6 since we have 36 characters.
+    """
+    char1 = ["A", "B", "C", "D", "E", "F"]
+    char2 = ["G", "H", "I", "J", "K", "L"]
+    char3 = ["M", "N", "O", "P", "Q", "R"]
+    char4 = ["S", "T", "U", "V", "W", "X"]
+    char5 = ["Y", "Z", "0", "1", "2", "3"]
+    char6 = ["4", "5", "6", "7", "8", "9"]
+    return [char1, char2, char3, char4, char5, char6]
