75fadb6933a1fc75617a9979b6d05d2f12fb9650
Hroddric <rodrigo.perezordoyobellido@gmail.com>
2018-07-17 12:03:09 +0100

    Added LDA and changed probing system.

--- erp.py
+++ erp.py
@@ -5,7 +5,7 @@ from classes import ERPDatasetCustom as ERPDC
 import glob
 import sklearn
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from functions import rowcol_paradigm
+from functions import rowcol_paradigm, dataset_probe, compact_dataset
 import random
 
 ## MAIN ##
@@ -17,30 +17,15 @@ for file_ in path_list:
     erp_data_list.append(ERPDC(file_))
 
 # Create a list containing the characters used in the speller
-chars = rowcol_paradigm()
-
-def dataset_probe(dataset):
-    print("! DATASET: !")
-
-    feat = dataset["features"]
-    rc = dataset["rowcol"]
-    flg = dataset["flags"]
-
-    # Check the dataset's characteristics and print them out
-    print("Length of features, rowcol and flags: {0}, {1}, {2}".format(len(feat), len(rc), len(flg)))
-    print("Length of the first 5 feature arrays:")
-    for i in range(5):
-        print("\t Data item number {0} has length {1}".format(i, len(feat[i])))
-    print("\t Last data item number has length {0}".format(len(feat[-1])))
-    print("\n")
+chars = rowcol_paradigm() 
 
 # Load random dataset from the list and probe the training and test sets
 erp_set = erp_data_list[random.randint(0,len(erp_data_list))]
 
-trdat = erp_set.train_data
+trdat = compact_dataset(erp_set.train_data)
 dataset_probe(trdat)
 
-tsdat = erp_set.test_data
+tsdat = compact_dataset(erp_set.test_data)
 dataset_probe(tsdat)
 
 # Separate features, rowcol and flags
@@ -60,17 +45,27 @@ std_trflg = np.asarray(trflg)[std_bool_list]
 std_tsfeat = np.asarray(tsfeat)[std_bool_list]
 std_tsflg = np.asarray(tsflg)[std_bool_list]
 
+std_trfeat = np.array([np.array(item) for item in std_trfeat])
+std_tsfeat = np.array([np.array(item) for item in std_tsfeat])
+
 # Check the length of these fellow arrays
 print("Length of the standarised training feature and flags arrays: {0} ; {1}".format(len(std_trfeat), len(std_trflg)))
 print("Length of the first item of the std training feature list: {0}".format(len(std_trfeat[0])))
+print("Type of the features: {0}, {1}, {2}".format(type(std_trfeat), type(std_trfeat[0]), type(std_trfeat[0][0])))
 print("Type of the flags: {0}".format(type(std_trflg[0])))
 print("Length of the standarised test feature and flags arrays: {0} ; {1}".format(len(std_tsfeat), len(std_tsflg)))
 print("Length of the first item of the std test feature list: {0}".format(len(std_tsfeat[0])) + "\n")
+print("Size of this stuff: {1}, {0}".format(std_trflg.shape, std_trfeat.shape))
 
 for i in range(len(std_trfeat)):
     if len(std_trfeat[i]) != std_len:
         raise ValueError("WARNING: SOMETHING IS WRONG WITH DIMENSIONS")
+    if type(std_trflg[i]) != np.float64:
+        raise ValueError("nonfloat")
 
 # Create LDA model
 lda = LDA()
-lda.fit(np.asarray(std_trfeat), np.asarray(std_trflg))
+lda.fit(std_trfeat, std_trflg)
+prediction = lda.predict(std_tsfeat)
+print(prediction - std_tsflg)
+print(trflg)
